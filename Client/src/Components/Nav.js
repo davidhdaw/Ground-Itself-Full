48ec362f48cd2f7616871b72b88ba6804dba8ab0
David Daw <daviddaw@gmail.com>
2022-10-03 10:54:48 -0700

    Adds initial component architecture

---
+++ Client/src/Components/Nav.js
@@ -0,0 +1,15 @@
+function Nav() {
+
+    return (
+      <header>
+        <p>Location</p>
+        <p>Length of Time</p>
+        <p>Phase of Play</p>
+        <p>How to Play</p>
+        <div className='userInfo'>User Info</div>    
+      </header>
+    );
+  }
+
+
+export default Nav
