2a705d01231962e9cefa242e33611194ba008274
David Daw <daviddaw@gmail.com>
2023-07-27 09:02:48 -0700

    fixes server?

--- Client/src/Components/Establishing.js
+++ Client/src/Components/Establishing.js
@@ -17,7 +17,7 @@ const phase3 = () => {
         <h2>Establishing Our Place</h2>
         <p>Players should take turns answering the prompts to establish the nature of our place. Keep going until the world feels established or there are no more questions, whichever happens first. Try to keep this discussion under 25 minutes. You may wish to make brief notes to jog your memory later</p>
         <h3>{game.establishingDrawn[game.establishingDrawn.length-1]}</h3>
-        <button className="newQuestionBtn" onClick={nextQuestion}>Next Question</button>
+        {game.establishingOptions.length !== 0 && <button className="newQuestionBtn" onClick={nextQuestion}>Next Question</button>}
         <button className="confirmLengthBtn" onClick={phase3}>End Phase</button>
         </div>
 
